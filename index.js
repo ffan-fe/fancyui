6f87be976e8c16ad2805cf367fc76775280f3684
xiaoshude <fzyde123@gmail.com>
2016-06-28 11:52:32 +0800

    回滚误删

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import Datepicker from './src/components/datepicker'
 import Rangepicker from './src/components/rangepicker'
 
+import Button from './src/components/button/';
+import Icon from './src/components/icon/';
+import Checkbox from './src/components/checkbox';
+
 'use strict';
 
 export default angular.module('Components', [
-  Datepicker,
-  Rangepicker
+  Button,
+  Icon,
+  Checkbox
 ]);
 
