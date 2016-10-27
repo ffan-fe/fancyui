aa1d9f3496ee012c4b45f9708a989e78699f095f
anyunfei <anyunfei@wanda.cn>
2016-10-27 15:13:01 +0800

    init popconfirm

--- examples/popconfirm/controller.js
+++ examples/popconfirm/controller.js
@@ -4,7 +4,16 @@ import 'prismjs/themes/prism.css';
 export default class popConfirmController {
   constructor($scope, Popconfirm, $sce) {
     'ngInject';
-    // this.Popconfirm = Popconfirm;
+    this.$scope = $scope;
+    this.Popconfirm = Popconfirm;
+  }
 
+  deleteConfirm() {
+    console.dir(this.Popconfirm)
+    this.instancePopConfirm();
+  }
+
+  instancePopConfirm() {
+    this.Popconfirm.test({title: '确定要删除吗?'})
   }
 }
