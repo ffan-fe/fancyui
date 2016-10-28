9901060e48e832c34a83d29a1a0b51d8ad737c54
anyunfei <anyunfei@wanda.cn>
2016-10-28 16:06:57 +0800

    init popconfirm

--- src/components/popconfirm/popconfirm.js
+++ src/components/popconfirm/popconfirm.js
@@ -40,45 +40,34 @@ export default class Popconfirm {
 	}
 
 	pop(param, e) {
-		// this.title = param.title;
-		let data = Object.assign(this.$rootScope.$new(), param);
-
 		let popDom;
-		if ($('.ant-popover').length == 0) {
-			popDom = this.$compile(template)(data);
-			this.$document.find('body').append(popDom);
-		} else {
-			popDom = $('.ant-popover');
+		let data = Object.assign(this.$rootScope.$new(), param);
+		let selector = $('.ant-popover');
+
+		if (selector.length != 0) {
+			selector.remove();
 		}
 
-		if (popDom[0].style.display == 'block') {
-			popDom[0].style.display = 'none';
-			popDom.removeClass('zoom-big-enter');
-			popDom.addClass('zoom-big-leave');
-		} else {
-			popDom[0].style.display = 'block';
-			popDom.addClass('zoom-big-enter');
-			// popDom.addClass('zoom-big-enter-active');
-		}
-
-
-
+		popDom = this.$compile(template)(data);
+		popDom.addClass('zoom-big-enter');
+		this.$document.find('body').append(popDom);
 
 
 		let eTop = e.target.offsetTop + e.target.offsetParent.offsetTop;
 		let popHeight = popDom[0].offsetHeight;
+		// let scrollTop = window.scrollTop;
 		let popTop = eTop - popHeight - 10;
 
 		let eLeft = e.target.offsetLeft + e.target.offsetParent.offsetLeft;
 		let eWidth = e.target.offsetWidth;
 		let popWidth = popDom[0].offsetWidth;
-		let popLeft = eLeft - popWidth / 2 + eWidth / 2;
+		let popLeft = eLeft - popWidth / 2;
 
 		switch (param.placement) {
 			case 'top':
 				popDom.css('top', popTop + 'px');
 				popDom.css('left', popLeft + 'px');
-				popDom.css('transform-origin', '50% 96px 0px');
+				// popDom.css('transform-origin', '50% 96px 0px');
 				break;
 			case 'left':
 
@@ -93,13 +82,24 @@ export default class Popconfirm {
 		}
 
 		$(document).bind('click',function(){
-			popDom[0].style.display = 'none'
+			popDom.remove();
 		});
 
-		$('.ant-popover').bind('click',function(e){
+		popDom.bind('click',function(e){
 			stopPropagation(e);
 		});
 
 		stopPropagation(e);
+
+		document.getElementById('btnConfirm').onclick =  function () {
+			param.onConfirm();
+			popDom.remove();
+		};
+
+		document.getElementById('btnCancel').onclick =  function () {
+			param.onCancel();
+			popDom.remove();
+		};
+
 	}
 }
