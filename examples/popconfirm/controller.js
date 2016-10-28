6ed6c960625886696aa6dac3f3af42ead8dc4abd
anyunfei <anyunfei@wanda.cn>
2016-10-28 13:32:44 +0800

    init popconfirm

--- examples/popconfirm/controller.js
+++ examples/popconfirm/controller.js
@@ -2,21 +2,20 @@ import Prism from 'prismjs';
 import 'prismjs/themes/prism.css';
 
 export default class popConfirmController {
-  constructor($scope, Popconfirm, $sce) {
-    'ngInject';
-    this.$scope = $scope;
-    this.Popconfirm = Popconfirm;
-  }
+	constructor($scope, Popconfirm, $sce) {
+		'ngInject';
+		this.$scope = $scope;
+		this.Popconfirm = Popconfirm;
+	}
 
-  deleteConfirm(e) {
-    console.dir(this.Popconfirm)
-    this.instancePopConfirm(e);
-  }
+	deleteConfirm(e) {
+		this.instancePopConfirm(e);
+	}
 
-  instancePopConfirm(e) {
-    this.Popconfirm.pop({
-      title: '确定要删除吗?',
-      placement: 'top'
-    }, e)
-  }
+	instancePopConfirm(e) {
+		this.Popconfirm.pop({
+			title: '确定要删除吗?',
+			placement: 'top'
+		}, e)
+	}
 }
--- src/components/popconfirm/popconfirm.js
+++ src/components/popconfirm/popconfirm.js
@@ -14,46 +14,66 @@ import './popconfirm.less';
  * @extends {Component}
  */
 export default class Popconfirm {
-  constructor($document, $compile) {
-    'ngInject'
-    // this.$scope = $scope;
-    this.$compile = $compile;
-    this.$document = $document;
-  }
+	constructor($document, $compile) {
+		'ngInject'
+		// this.$scope = $scope;
+		this.$compile = $compile;
+		this.$document = $document;
+	}
 
-  /**
-   * @override
-   */
-  _initDefaultValue() {
-  }
+	/**
+	 * @override
+	 */
+	_initDefaultValue() {
+	}
 
-  /**
-   * @override
-   */
-  _createClassName() {
-  }
+	/**
+	 * @override
+	 */
+	_createClassName() {
+	}
 
-  /**
-   * @override
-   */
-  _launch() {
-  }
+	/**
+	 * @override
+	 */
+	_launch() {
+	}
 
-  pop(param, e) {
-    console.log(e.target.offsetTop)
-    let pop = this.$compile(template)(param);
-    pop.css('display', 'block');
+	pop(param, e) {
+		let popDom;
+		if ($('.ant-popover').length == 0) {
+			popDom = this.$compile(template)(param);
+		} else {
+			popDom = $('.ant-popover');
+		}
 
-    this.$document.find('body').append(pop);
-    console.log(param.placement);
+		if (popDom[0].style.display == 'block') {
+			popDom[0].style.display = 'none'
+		} else {
+			popDom[0].style.display = 'block'
+		}
 
-    switch (param.placement) {
-      case 'top':
+		this.$document.find('body').append(popDom);
 
-        break;
-      case 'left':
 
-        break;
-    }
-  }
+		let eTop = e.target.offsetTop + e.target.offsetParent.offsetTop;
+		let popHeight = popDom[0].offsetHeight;
+		let popTop = eTop - popHeight - 10;
+
+		let eLeft = e.target.offsetLeft + e.target.offsetParent.offsetLeft;
+		let eWidth = e.target.offsetWidth;
+		let popWidth = popDom[0].offsetWidth;
+		let popLeft = eLeft - popWidth / 2 + eWidth / 2;
+
+		switch (param.placement) {
+			case 'top':
+				popDom.css('top', popTop + 'px');
+				popDom.css('left', popLeft + 'px');
+				popDom.css('transform-origin', '50% 96px 0px');
+				break;
+			case 'left':
+
+				break;
+		}
+	}
 }
