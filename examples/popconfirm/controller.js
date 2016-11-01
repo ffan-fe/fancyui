e937a738dd45a4783077be7ec73ce45b945e77f9
anyunfei <anyunfei@wanda.cn>
2016-11-01 14:46:26 +0800

    init popconfirm

--- examples/popconfirm/controller.js
+++ examples/popconfirm/controller.js
@@ -8,7 +8,7 @@ export default class popConfirmController {
 		this.Popconfirm = Popconfirm;
 	}
 
-	topLeftConfirm(e) {
+	topLeftPop(e) {
 		this.Popconfirm.pop({
 			title: '确定要删除吗?',
 			placement: 'top',
@@ -23,10 +23,55 @@ export default class popConfirmController {
 		}, e)
 	}
 
-	topConfirm(e) {
+	topPop(e) {
 		this.Popconfirm.pop({
 			title: '确定要删除top吗?',
 			placement: 'top',
+			okText: '确定',
+			cancelText: '取消',
+			onConfirm: function () {
+				console.log('确定');
+			},
+			onCancel: function () {
+				console.log('取消');
+			}
+		}, e)
+	}
+
+	leftPop(e) {
+		this.Popconfirm.pop({
+			title: '确定要删除left吗?',
+			placement: 'left',
+			okText: '确定',
+			cancelText: '取消',
+			onConfirm: function () {
+				console.log('确定');
+			},
+			onCancel: function () {
+				console.log('取消');
+			}
+		}, e)
+	}
+
+	bottomPop(e) {
+		this.Popconfirm.pop({
+			title: '确定要删除bottom吗?',
+			placement: 'bottom',
+			okText: '确定',
+			cancelText: '取消',
+			onConfirm: function () {
+				console.log('确定');
+			},
+			onCancel: function () {
+				console.log('取消');
+			}
+		}, e)
+	}
+
+	rightPop(e) {
+		this.Popconfirm.pop({
+			title: '确定要删除right吗?',
+			placement: 'right',
 			okText: '确定',
 			cancelText: '取消',
 			onConfirm: function () {
