00bebf0449939ac62dc239e3832cfaae8c82d3e9
chuxin1 <chuxin1@wanda.cn>
2016-11-08 10:00:26 +0800

    delete index.html

--- webpack.example.config.js
+++ webpack.example.config.js
@@ -31,7 +31,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './examples/index.html',
       inject: 'body',
       minify: false
     }),
