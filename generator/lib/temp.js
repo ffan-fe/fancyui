042a8711122f1b69cedb5cf523afe23dc4b053f0
guanbingchang <guanbingchang@wanda.cn>
2017-01-03 10:39:07 +0800

    update generator

--- generator/lib/temp.js
+++ generator/lib/temp.js
@@ -6,12 +6,9 @@
  * @example: http://www.fancyui.org/#/zh-cn/component/<%= name %>
  */
 
-import angular from 'angular';
-import uiRouter from 'angular-ui-router';
 import <%= name %>Component from './<%= name %>.component';
 
 let <%= name %>Module = angular.module('<%= name %>', [
-  uiRouter
 ])
 
 .component('<%= name %>', <%= name %>Component);
