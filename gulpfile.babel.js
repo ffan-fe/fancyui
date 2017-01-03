ccd79c31a1a9f3e2f9de172f077b43e0a7e3e818
guanbingchang <guanbingchang@wanda.cn>
2017-01-03 18:38:34 +0800

    watch lib: fancyui的lib更新，会直接刷新example的网站

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,6 +6,7 @@ import rename from 'gulp-rename';
 import yargs    from 'yargs';
 import rimraf   from 'rimraf';
 import merge    from 'merge-stream';
+import watch    from 'gulp-watch';
 
 'use strict';
 
@@ -13,7 +14,7 @@ let resolveToLibs = (glob = '') => {
   return path.join('lib/', glob); // app/components/{glob}
 };
 
-let resolveToComponents = (glob = '')=>{
+let resolveToComponents = (glob = '') => {
   return path.join('example/client/app/components/', glob); // app/components/{glob}
 }
 
@@ -24,39 +25,34 @@ gulp.task('component', () => {
   const name = yargs.argv.name;
 
   const destComponentsPath = path.join(resolveToComponents(), name);
-  const destLibPaths = path.join(resolveToLibs(),name);
+  const destLibPaths = path.join(resolveToLibs(), name);
 
   let gulpLibs = gulp.src(path.join(__dirname, 'generator', 'lib/**/*.**'))
     .pipe(template({
       name: name,
       upCaseName: cap(name),
-      dateTime:new Date()
+      dateTime: new Date()
     }))
     .pipe(rename((path) => {
       path.basename = path.basename.replace('temp', name);
     }))
     .pipe(gulp.dest(destLibPaths));
-  
+
   let gulpComponents = gulp.src(path.join(__dirname, 'generator', 'example/component/*.**'))
     .pipe(template({
       name: name,
       upCaseName: cap(name),
-      dateTime:new Date()
+      dateTime: new Date()
     }))
     .pipe(rename((path) => {
       path.basename = path.basename.replace('temp', name);
     }))
     .pipe(gulp.dest(destComponentsPath));
 
-  return merge(gulpLibs,gulpComponents);
-    
+  return merge(gulpLibs, gulpComponents);
 });
 
-gulp.task('build:demo',()=>{
-  rimraf('example/node_modules/fancyui/lib/**',
-    ()=>{
-      gulp.src('lib/**')
-      .pipe(gulp.dest('example/node_modules/fancyui/lib'))
-    }
-  );
-})
+gulp.task('build:demo', () => {
+  return watch('lib/**')
+    .pipe(gulp.dest('example/node_modules/fancyui/lib'))
+});
