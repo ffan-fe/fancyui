2fe650ff5f6a00dfe2375b78854fbe2ca978133e
guanbingchang <guanbingchang@wanda.cn>
2017-01-04 18:07:05 +0800

    add

--- example/client/app/components/select/select.controller.js
+++ example/client/app/components/select/select.controller.js
@@ -1,7 +1,7 @@
 /**
- * @description xxxx
+ * @description select component
  *
- * @author XXXXX@wandan.cn
+ * @author guanbingchang 148135244@qq.com
  * @date Tue Jan 03 2017 13:28:41 GMT+0800 (CST)
  * @example: http://www.fancyui.org/#/zh-cn/component/select
  */
@@ -9,7 +9,51 @@
 export default class SelectController {
   constructor($state) {
     'ngInject'
-    this.$state= $state;
+    this.$state = $state;
     this.name = 'select';
+
+    this.city = {
+      id: '1001',
+      label: 'bjtz',
+      name: '北京通州万达广场'
+    };
+
+    this.data = [
+      {
+        title: '热门广场',
+        options: [{
+            id: '1001',
+            label: 'bjtz',
+            name: '北京通州万达广场'
+          },
+          {
+            id: '1002',
+            label: 'shjq',
+            name: '上海江桥万达广场'
+          },
+          {
+            id: '1003',
+            label: 'shzp',
+            name: '上海周浦万达广场'
+          }]
+      },
+      {
+        title: '其他广场',
+        options: [{
+          id: '1004',
+          label: 'hf',
+          name: '合肥万达广场'
+        },
+          {
+            id: '1005',
+            label: 'hf',
+            name: '武汉万达广场'
+          }]
+      }
+    ]
+  }
+
+  click(){
+    console.log(this.city);
   }
 }
