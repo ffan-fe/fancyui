4cf0b1f1e209ccf07a35131d5164a70a00591ca0
guanbingchang <guanbingchang@wanda.cn>
2017-01-06 08:52:23 +0800

    add isparta-lodaer for coverage clearly

--- karma.conf.js
+++ karma.conf.js
@@ -1,85 +1,89 @@
-var config = 
+var path = require('path');
 
-module.exports = function (config) {
-  config.set({
-    // base path used to resolve all patterns
-    basePath: '',
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai'],
+var config =
+  module.exports = function (config) {
+    config.set({
+      // base path used to resolve all patterns
+      basePath: '',
 
-    // list of files/patterns to load in the browser
-    files: [
-      {
-        pattern: 'node_modules/angular/angular.js'
+      // frameworks to use
+      // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+      frameworks: ['mocha', 'chai'],
+
+      // list of files/patterns to load in the browser
+      files: [
+        {
+          pattern: 'spec.bundle.js', watched: false
+        }
+      ],
+
+      plugins: [
+        require("karma-chai"),
+        require("karma-chrome-launcher"),
+        require("karma-mocha"),
+        require("karma-mocha-reporter"),
+        require("karma-sourcemap-loader"),
+        require("karma-coverage"),
+        require("karma-webpack")
+      ],
+
+      // preprocess matching files before serving them to the browser
+      // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+      preprocessors: {
+        //'lib/**/!(*.spec).js':['webpack', 'sourcemap', 'coverage'],
+        'spec.bundle.js': ['webpack', 'sourcemap']
       },
-      {
-        pattern: 'node_modules/angular-mocks/angular-mocks.js'
+
+      webpack: {
+        devtool: 'inline-source-map',
+        module: {
+          preLoaders: [{
+            test: /\.js$/,
+            include: path.resolve('lib/'),
+            loader: 'isparta'
+          }],
+          loaders: [
+            { test: /\.js/, exclude: [/app\/bin/, /node_modules/], loader: 'babel-loader?cacheDirectory' },
+            { test: /\.html$/, loader: 'raw-loader' },
+            { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
+            { test: /\.css$/, loader: 'style-loader!css-loader' }
+          ]
+        }
       },
-      {
-        pattern: 'lib/**/**.spec.js', watched: false
-      }
-    ],
 
-    plugins: [
-      require("karma-chai"),
-      require("karma-chrome-launcher"),
-      require("karma-mocha"),
-      require("karma-mocha-reporter"),
-      require("karma-sourcemap-loader"),
-      require("karma-coverage"),
-      require("karma-webpack")
-    ],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: { 
-      //'lib/**/!(*.spec).js':['webpack', 'sourcemap', 'coverage']
-      'lib/**/**.spec.js': ['webpack', 'sourcemap', 'coverage'] 
-    },
-
-    webpack: {
-      devtool: 'inline-source-map',
-      module: {
-        loaders: [
-          { test: /\.js/, exclude: [/app\/bin/, /node_modules/], loader: 'babel-loader?cacheDirectory' },
-          { test: /\.html$/, loader: 'raw-loader' },
-          { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
-          { test: /\.css$/, loader: 'style-loader!css-loader' }
+      coverageReporter: {
+        dir: 'coverage/',
+        reporters: [
+          { type: 'text-summary' },
+          { type: 'html' }
         ]
-      }
-    },
+      },
 
-    coverageReporter: {
-      type: 'html',
-      dir: 'coverage/'
-    },
+      webpackServer: {
+        noInfo: 'errors-only' // prevent console spamming when running in Karma!
+      },
 
-    webpackServer: {
-      noInfo: true // prevent console spamming when running in Karma!
-    },
+      reporters: ['mocha', 'coverage'],
 
-    reporters: ['mocha', 'coverage'],
+      // web server port
+      port: 9876,
 
-    // web server port
-    port: 9876,
+      // enable colors in the output
+      colors: true,
 
-    // enable colors in the output
-    colors: true,
+      // level of logging
+      // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+      logLevel: config.LOG_INFO,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+      // toggle whether to watch files and rerun tests upon incurring changes
+      autoWatch: false,
 
-    // toggle whether to watch files and rerun tests upon incurring changes
-    autoWatch: false,
+      // start these browsers
+      // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+      browsers: ['Chrome'],
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-    // if true, Karma runs tests once and exits
-    singleRun: true
-  });
-};
+      // if true, Karma runs tests once and exits
+      singleRun: true
+    });
+  };
