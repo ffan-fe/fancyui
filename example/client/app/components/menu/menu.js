a183111cf71e9b18979ed4a84d6b461d6013f59e
xiaoshude <fzyde123@gmail.com>
2017-01-06 09:58:20 +0800

    add test

--- example/client/app/components/menu/menu.js
+++ example/client/app/components/menu/menu.js
@@ -1,4 +1,7 @@
-import fMenuModule from '../../common/menu/menu.js'
+import fMenu from 'fancyui/lib/menu/menu.js';
 import menuComponent from './menu.component';
-export default angular.module('menu', [])
-.component('menu', menuComponent);
+export default angular.module('menuDemo', [
+    fMenu.name
+  ]
+)
+  .component('menu', menuComponent);
