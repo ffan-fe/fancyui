1b4c6aa6b60893543ea3128e12ff3846308ce8e1
guanbingchang <guanbingchang@wanda.cn>
2017-01-22 10:46:07 +0800

    add checkboxtree default-collage

--- example/app/components/checkboxtree/checkboxtree.js
+++ example/app/components/checkboxtree/checkboxtree.js
@@ -1,9 +1,7 @@
 import checkboxtreeComponent from './checkboxtree.component.js';
-import 'angular-ui-tree';
 import checkboxtree from '../../../../lib/checkboxtree/index.js';
 
 export default angular.module('democheckboxtree', [
-  'ui.tree',
   checkboxtree.name
 ])
 .component('democheckboxtree', checkboxtreeComponent);
