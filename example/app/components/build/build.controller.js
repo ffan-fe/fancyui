92cba1af68a36000c7f279d8a26d8e7402b282dd
guanbingchang <guanbingchang@wanda.cn>
2017-01-23 08:48:38 +0800

    update structure page

--- example/app/components/build/build.controller.js
+++ example/app/components/build/build.controller.js
@@ -3,13 +3,14 @@
  *
  * @author yourname
  */
-
+import Base from '../../base';
 import initImage from './images/init.png';
 import startImage from './images/start.png';
 
-export default class BuildController {
-	constructor($interval, $timeout) {
+export default class BuildController extends Base{
+	constructor($interval, $timeout,$state,$translate) {
 		'ngInject';
+		super($state,$translate)
 		this.initImage = initImage;
 		this.startImage = startImage;
 	}
