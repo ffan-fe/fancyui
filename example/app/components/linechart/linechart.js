ca40aabf9dd9deb83333eb361b333d56225a97ed
guanbingchang <guanbingchang@wanda.cn>
2017-02-16 09:27:23 +0800

    select focus unit test & ignorecase

--- example/app/components/linechart/linechart.js
+++ example/app/components/linechart/linechart.js
@@ -1,5 +1,5 @@
 import linechartComponent from './linechart.component.js';
-import Charts from '../../../../lib/charts/index.js';
+import Charts from '../../../../lib/Charts/index.js';
 
 export default angular.module('lineChart', [
 		Charts.name
