1de9f593b5595d070622160058c19bdf31ba8cb9
guanbingchang <guanbingchang@wanda.cn>
2017-03-01 16:55:37 +0800

    fix some issues

--- example/app/components/nav/menu.js
+++ example/app/components/nav/menu.js
@@ -1,4 +1,4 @@
-import Menu from '../../../../lib/nav/index.js';
+import Menu from '../../../../lib/Nav/index.js';
 import menuComponent from './menu.component.js';
 import Service from '../../../../lib/Service/index.js';
 
