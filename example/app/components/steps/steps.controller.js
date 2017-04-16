bf3e9eca858227380b988ff34cc070584669a031
xiaoshude <fzyde123@gmail.com>
2017-04-16 14:36:58 +0800

    add steps

--- example/app/components/steps/steps.controller.js
+++ example/app/components/steps/steps.controller.js
@@ -12,8 +12,29 @@ export default class StepsController {
 
     this.stepsData = [
       {
-
+        title: '总览',
+        iconContent: '1'
+      },
+      {
+        title: '设计',
+        iconContent: '2'
+      },
+      {
+        title: '施工',
+        iconContent: '3'
+      },
+      {
+        title: '验收',
+        iconContent: '4'
+      },
+      {
+        title: '发布',
+        icon: `<span class="glyphicon glyphicon-play-circle"></span>`
       }
     ];
+
+    this.onClick = (item, index) => {
+      console.log(item, index);
+    }
   }
 }
